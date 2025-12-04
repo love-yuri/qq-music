792e8c9b5267f252570522ffb7594f05f2aa0894
love-yuri <yuri2078170658@gmail.com>
2025-12-04 20:04:27 +0800

    feat: 添加基础布局

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #include <QApplication>
 #include "mainwindow.hpp"
-#include "utils/common/log_macros.hpp"
 
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   MainWindow window;
-  yuri::Log() << "yuri is yes";
   return QApplication::exec();
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,10 +1,17 @@
 #include "mainwindow.hpp"
 #include "ui/ui_mainwindow.h"
 #include <QMainWindow>
+#include <QFile>
+#include "yuri_log.hpp"
 
 MainWindow::MainWindow(QMainWindow *parent) :
   QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+  QFile file(":/qss/main.qss");
+  if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+    setStyleSheet(styleSheet() + file.readAll());
+  }
+
   show();
 }
 
