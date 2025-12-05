7d3ee2003aebc3a542997b03f0c7a99b36bcdc0c
love-yuri <yuri2078170658@gmail.com>
2025-12-05 16:51:04 +0800

    feat: 添加示例，同步api修改

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <QApplication>
 #include "mainwindow.hpp"
+#include "api/play_list.hpp"
+import thread_pool;
+
+#include <curl/curl.h>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
   QApplication a(argc, argv);
+
+  global_config.qq = "2078170658";
+  thread_manager->init();
+  thread_manager->addTask([] {
+    for (const auto res = qqmusic_api::playlist::get_user_playlists(); const auto &i: res.data.disslist) {
+      yuri::Log() << "name: " << i.diss_name << " tid: " << i.tid;
+    }
+  });
+
   MainWindow window;
   return QApplication::exec();
 }
