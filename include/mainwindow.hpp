c86ad03f9c5687d6a92265113cd021b193a1ed2a
love-yuri <yuri2078170658@gmail.com>
2025-12-07 20:16:16 +0800

    feat: 添加ui界面

--- include/mainwindow.hpp
+++ include/mainwindow.hpp
@@ -4,26 +4,36 @@
  * @LastEditTime: 2025-11-11 20:32:09
  * @Description:
  */
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
+#include <QHBoxLayout>
 #include <QMainWindow>
+#include <QMouseEvent>
+#include <QLabel>
+#include "yuri_log.hpp"
+#include "components/nav_menu_item.hpp"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
 class MainWindow;
 }
-
 QT_END_NAMESPACE
 
 class MainWindow final : public QMainWindow {
   Q_OBJECT
+
 public:
   explicit MainWindow(QMainWindow *parent = nullptr);
   ~MainWindow() override;
 
 private:
   Ui::MainWindow *ui;
+  NavMenuItem* active_menu_item = nullptr;
+  std::vector<NavMenuItem*> items {
+    new NavMenuItem("fm", "我的播客"),
+    new NavMenuItem("play", "发现音乐"),
+    new NavMenuItem("friend", "发现音乐"),
+    new NavMenuItem("video", "发现音乐"),
+    new NavMenuItem("my-likes", "我的喜欢")
+  };
 };
-
-#endif
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -2,14 +2,32 @@
 #include "ui/ui_mainwindow.h"
 #include <QMainWindow>
 #include <QFile>
+#include <QPainter>
 #include "yuri_log.hpp"
+#include "components/nav_menu_item.hpp"
+#include <QTimer>
 
 MainWindow::MainWindow(QMainWindow *parent) :
   QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
-  QFile file(":/qss/main.qss");
-  if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+  if (QFile file(":/qss/main.qss"); file.open(QIODevice::ReadOnly | QIODevice::Text)) {
     setStyleSheet(styleSheet() + file.readAll());
+  }
+
+  // 左侧菜单默认显示300px
+  QTimer::singleShot(0, [this] {
+    ui->splitter->setSizes({300, ui->splitter->width() - 300});
+  });
+
+  for (auto &item : items) {
+    ui->verticalLayout_2->addWidget(item);
+    connect(item, &NavMenuItem::clicked, [item, this] {
+      if (active_menu_item) {
+        active_menu_item->set_active(false);
+      }
+      item->set_active(true);
+      active_menu_item = item;
+    });
   }
 
   show();
