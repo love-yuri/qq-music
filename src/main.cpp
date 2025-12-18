d2831b4af50622bf91968279b749b47f906b0258
love-yuri <yuri2078170658@gmail.com>
2025-12-18 21:07:32 +0800

    feat: 设置全局默认字体

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ import thread_pool;
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
 
+  QApplication::setFont(QFont("Maple Mono NF CN Medium"));
   MainWindow window;
   window.show();
 
